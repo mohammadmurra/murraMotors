79a9b3ff76a2992936e2ba47ebbcdf6f15f40d12
mohammad murra <41515454+mohammadmurra@users.noreply.github.com>
2024-03-20 02:20:49 +0200

    enhance add employee form

--- src/pages/management/Employees/AddEmployee/index.jsx
+++ src/pages/management/Employees/AddEmployee/index.jsx
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   Card,
   Button,
   Grid,
-  Divider,
   FormControl,
   InputLabel,
   Select,
@@ -17,14 +16,14 @@ import IconButton from '@mui/material/IconButton';
 import Box from '@mui/material/Box';
 import AppAnimate from '../../../../@crema/core/AppAnimate';
 import TextField from '@mui/material/TextField';
-import { Fonts } from '../../../../shared/constants/AppEnums';
+import {Fonts} from '../../../../shared/constants/AppEnums';
 
 import {
   addEmployees,
   generateEID,
 } from '../../../../@crema/services/auth/firebase/firebase';
-import { LoadingButton } from '@mui/lab';
-import { AppConfirmDialog } from '@crema';
+import {LoadingButton} from '@mui/lab';
+import {AppConfirmDialog} from '@crema';
 const AddEmployee = ({
   // title,
   description,
@@ -86,14 +85,17 @@ const AddEmployee = ({
 
   return (
     <form onSubmit={(e) => addEmployee(e)}>
-      {console.log(error)}
-      <AppConfirmDialog open={error} title={error?.message} onConfirm={() => setError()} onDeny={() => setError()} />
+      <AppConfirmDialog
+        open={!!error}
+        title={error?.message || ''}
+        onConfirm={() => setError(null)}
+        onDeny={() => setError(null)}
+      />
       <AppAnimate animation='transition.slideUpIn' delay={200}>
         <Card>
           <CardHeader
             sx={{
               py: 4,
-
               pb: 1,
               px: 5,
               display: 'flex',
@@ -106,26 +108,15 @@ const AddEmployee = ({
                 marginBottom: 0.25,
               },
             }}
-            // title={title}
             subheader={description}
-            root={{
-              subheader: {
-                fontSize: 13,
-              },
-            }}
             action={
               source ? (
                 <Box>
                   <IconButton
                     aria-label='view code'
                     onClick={() => {
-                      if (animation) {
-                        setAnimation(!animation);
-                        setTimeout(() => setToggleViewSource(!viewSource), 400);
-                      } else {
-                        setAnimation(!animation);
-                        setToggleViewSource(!viewSource);
-                      }
+                      setAnimation(!animation);
+                      setTimeout(() => setToggleViewSource(!viewSource), 400);
                     }}
                     size='large'
                   >
@@ -135,23 +126,20 @@ const AddEmployee = ({
               ) : null
             }
           />
-
-          <CardContent sx={{ px: 10, pt: 1 }}>
-            <Grid container item xs={12} sm={5} spacing={4} textAlign={'start'}>
-              <Grid item textAlign={'start'} xs={12} lg={6}>
+          <CardContent>
+            <Grid container spacing={3}>
+              <Grid item xs={12} sm={6}>
                 <TextField
-                  id='outlined-basic'
+                  fullWidth
+                  id='name'
                   name='name'
-                  label={['Full Name']}
+                  label='Full Name'
                   variant='outlined'
                   onChange={handleChange}
                   required
                 />
               </Grid>
-
-          
-
-              <Grid item textAlign={'start'} xs={9} lg={5} mr={11}>
+              <Grid item xs={12} sm={6}>
                 <FormControl fullWidth>
                   <InputLabel id='demo-simple-select-label'>Role</InputLabel>
                   <Select
@@ -159,99 +147,91 @@ const AddEmployee = ({
                     id='role'
                     name='role'
                     value={role}
-                    label={['Role']}
+                    label='Role'
                     onChange={handleChangeRole}
+                    required
                   >
                     <MenuItem value={'admin'}>Admin</MenuItem>
-                    <MenuItem value={'accounting_manager'}>Accounting Manager</MenuItem>
+                    <MenuItem value={'accounting_manager'}>
+                      Accounting Manager
+                    </MenuItem>
                     <MenuItem value={'call_support'}>Call Support</MenuItem>
-                    <MenuItem value={'warehouse_employee'}>Warehose Empoyee</MenuItem>
+                    <MenuItem value={'warehouse_employee'}>
+                      Warehose Empoyee
+                    </MenuItem>
                     <MenuItem value={'sales_man'}>Sales Man</MenuItem>
                     <MenuItem value={'desginer'}>Desginer</MenuItem>
                     <MenuItem value={'data_entry'}>Data Entry</MenuItem>
-                    <MenuItem value={'delivery_employee'}>Delivery Employee</MenuItem>
+                    <MenuItem value={'delivery_employee'}>
+                      Delivery Employee
+                    </MenuItem>
                     <MenuItem value={'developer'}>Developer</MenuItem>
-                    <MenuItem value={'human_resources'}>Human Resources</MenuItem>
+                    <MenuItem value={'human_resources'}>
+                      Human Resources
+                    </MenuItem>{' '}
                   </Select>
                 </FormControl>
               </Grid>
-              <Grid item textAlign={'start'} xs={12} lg={6}>
+              <Grid item xs={12} sm={6}>
                 <TextField
+                  fullWidth
                   type='number'
                   id='Phone_Number'
                   name='Phone_Number'
-                  label={['Phone Number']}
+                  label='Phone Number'
                   variant='outlined'
                   onChange={handleChange}
                   required
                 />
               </Grid>
-              <Grid item textAlign={'start'} xs={12} lg={6}>
+              <Grid item xs={12} sm={6}>
                 <TextField
+                  fullWidth
                   id='email'
                   name='email'
-                  label={['Email']}
+                  label='Email'
                   variant='outlined'
                   onChange={handleChange}
                   required
                 />
               </Grid>
-              <Grid item textAlign={'start'} xs={12} lg={6}>
+              <Grid item xs={12} sm={6}>
                 <TextField
+                  fullWidth
                   id='password'
                   name='password'
-                  label={['Password']}
+                  label='Password'
                   type='password'
                   variant='outlined'
                   onChange={handleChange}
                   required
                 />
               </Grid>
-
-              <Grid item xs={12} style={{ minHeight: '100px' }}>
+              <Grid item xs={12} sm={6}>
                 <TextField
-                  style={{ width: '100%' }}
+                  fullWidth
                   id='address'
                   name='address'
-                  label={['Address']}
+                  label='Address'
                   variant='outlined'
-                  size='large'
-                  inputProps={{ style: { minHeight: 70 } }}
+                  multiline
+                  rows={4}
                   onChange={handleChange}
                   required
                 />
               </Grid>
-
-              <Grid item sx={0} sm={1}>
-                <Divider
-                  orientation='horizontal'
-                  style={{
-                    marginRight: '50%',
-                    marginLeft: '50%',
-                    height: '100%',
-                    width: '2px',
-                  }}
-                />
-              </Grid>
-
-              <Grid container xs={12} sm={6} item spacing={2}>
-                <Grid
-                  item
-                  maxHeight={{ xs: '170px', lg: '250px' }}
-                  overflow={'auto'}
-                  xs={12}
-                ></Grid>
-              </Grid>
             </Grid>
-            {
-              isAdding ?
+            <Box mt={3} display='flex' justifyContent='flex-end'>
+              {isAdding ? (
                 <LoadingButton loading variant='outlined'>
                   Add Employee
-                </LoadingButton> :
+                </LoadingButton>
+              ) : (
                 <Button type='submit' variant='contained' color='primary'>
                   Add Employee
                 </Button>
-            }
+              )}
+            </Box>
           </CardContent>
         </Card>
       </AppAnimate>
